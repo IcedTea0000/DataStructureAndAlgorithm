140fd9bcfd80950fbd2fafd549d274f8055afbeb
Lam Ngo <ngohoanglam65@gmail.com>
2020-12-27 14:58:04 +0700

    add array data structure

--- arrays/my_array.py
+++ arrays/my_array.py
@@ -1,9 +1,9 @@
 """
-my implementation of list
+example manual implementation of array to understand how push, pop, delete, shift ... works
 """
 
 
-class MyList():
+class MyArray():
     def __init__(self):
         self.length = 0
         self.data = []
@@ -14,7 +14,7 @@ class MyList():
     def get(self, index):
         """
         O(1) -> O(c)
-        :param index: pointer to list
+        :param index: pointer to Array
         :return: value of object at pointer
         """
         return self.data[index]
@@ -22,8 +22,8 @@ class MyList():
     def push(self, element):
         """
         O(2) -> O(c)
-        add item to the end of list
-        :param element: object to add to list
+        add item to the end of Array
+        :param element: object to add to Array
         """
         self.data.append(element)
         self.length += 1
@@ -31,7 +31,7 @@ class MyList():
     def pop(self):
         """
         O(4) -> O(c)
-        return last item of list and delete from list
+        return last item of Array and delete from Array
         :return: last item
         """
         last_item = self.data[self.length - 1]
@@ -42,9 +42,9 @@ class MyList():
     def delete(self, index):
         """
         O(3) -> O(c)
-        delete item at index in list
+        delete item at index in Array
         :param index: index of item want to delete
-        :return: item at index in list
+        :return: item at index in Array
         """
         delete_item = self.data[index]
         self.lshift_items(index)
@@ -53,7 +53,7 @@ class MyList():
     def lshift_items(self, index):
         """
         O(n+2) -> O(n)
-        shift all items of list to left 1 index
+        shift all items of Array to left 1 index
         :param index: position to start shifting to left
         :return:
         """
@@ -64,7 +64,7 @@ class MyList():
 
 
 if __name__ == '__main__':
-    l1 = MyList()
+    l1 = MyArray()
     l1.push('test1')
     l1.push('test2')
     l1.push('test3')
